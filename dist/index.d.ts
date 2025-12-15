f3ee2d0f1bd9b013f5fbc930028683c3e9b7bdde
Enes Erk <enes.erk@avency.de>
2025-12-15 15:26:37 +0100

    fix: remove dist folder

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { MultiSelect } from "./MultiSelect";
-import { MultiSelectPlaceholderTypeEnum } from "./MultiSelectPlaceholderTypeEnum";
-export { MultiSelect, MultiSelectPlaceholderTypeEnum };
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-import { MultiSelect } from "./MultiSelect";
-import { MultiSelectPlaceholderTypeEnum } from "./MultiSelectPlaceholderTypeEnum";
-export { MultiSelect, MultiSelectPlaceholderTypeEnum };
-//# sourceMappingURL=index.js.map
